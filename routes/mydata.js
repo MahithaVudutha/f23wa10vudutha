06306267134bc69172af5ea5458ca702efabb8f8
MahithaVudutha <vuduthamahitha@gmail.com>
2023-10-25 22:20:08 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mahitha Vudutha' });
+  res.render('mydata', { title: 'Mahitha Vudutha' });
 });
 
 module.exports = router;
